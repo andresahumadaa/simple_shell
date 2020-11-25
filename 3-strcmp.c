d871b0f1d5e0822ec1a25c76ca69c0971de5374f
andresahumadaa <2298@holbertonschool.com>
2020-11-25 19:22:36 +0000

    compara 2f

---
+++ 3-strcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strcmp - Compares two strings
+ *@s1: String 1
+ *@s2: String 2
+ * Return: return to compare strings
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+int a = 0;
+int b;
+while (s1[a] || s2[a])
+{
+if (s1[a] == s2[a])
+{
+b = 0;
+}
+else
+{
+b = s1[a] - s2[a];
+break;
+}
+a++;
+}
+return (b);
+}
