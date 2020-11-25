1a6d817c3559306e3bb18b6b3d60d34ff8b98882
andresahumadaa <2298@holbertonschool.com>
2020-11-25 03:51:22 +0000

    estilos mal

--- simple_shell.c
+++ simple_shell.c
@@ -7,10 +7,10 @@
  */
 int main(int argc, char *argv[])
 {
-  int exit_status = EXIT_SUCCESS;
-  /* Redirect Interrupt signal to handler function */
-  signal(SIGINT, handler);
-  exit_status = loop(argv[0]); /*infinite command loop*/
-  (void)argc;
-  return (exit_status);
+int exit_status = EXIT_SUCCESS;
+/* Redirect Interrupt signal to handler function */
+signal(SIGINT, handler);
+exit_status = loop(argv[0]); /*infinite command loop*/
+(void)argc;
+return (exit_status);
 }
